0a86b1ef3de47d8310536141f4a7cac67c89967e
andikaryan <andikaryan2002@gmail.com>
2023-10-02 21:05:08 +0700

    Update port

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get('/api/customers/:id', (req,res)=>{
     })
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`App is listening in port ${port}`)
 });
